2b79b3c1bdb3a6ac20be09a444b1cc3bbad32ab1
Sam Georges <sam@daftspunk.com>
2012-11-28 22:57:17 +1100

    Add cache to inflector

--- system/modules/phpr/helpers/phpr_inflector.php
+++ system/modules/phpr/helpers/phpr_inflector.php
@@ -1,14 +1,20 @@
 <?php
 
-class Phpr_Inflector
+class Phpr_Inflector 
 {
+    protected static $cache = array();
+
     /**
      * Pluralizes English nouns.
      * @param string $word English noun to pluralize
      * @return string Plural noun
      */
-    public static function pluralize($word)
+    public static function pluralize($word) 
     {
+        $result = self::get_cached_value('pluralize', $word);
+        if ($result)
+            return $result;
+
         $plural = array(
             '/(quiz)$/i' => '\1zes',
             '/^(ox)$/i' => '\1en',
@@ -28,48 +34,10 @@ class Phpr_Inflector
             '/(ax|test)is$/i'=> '\1es',
             '/s$/i'=> 's',
             '/$/'=> 's'
-        );
-        
-        $uncountable = array(
-            'equipment', 
-            'information', 
-            'rice', 
-            'money', 
-            'species', 
-            'series', 
-            'fish', 
-            'sheep'
-        );
+            );
 
-        $irregular = array(
-            'person' => 'people',
-            'man' => 'men',
-            'child' => 'children',
-            'sex' => 'sexes',
-            'move' => 'moves'
-        );
-
-        $lowercased_word = strtolower($word);
-
-        foreach ($uncountable as $_uncountable)
-        {
-            if (substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable)
-                return $word;
-        }
-
-        foreach ($irregular as $_plural=> $_singular)
-        {
-            if (preg_match('/('.$_plural.')$/i', $word, $arr)) 
-                return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word);
-        }
-
-        foreach ($plural as $rule => $replacement) 
-        {
-            if (preg_match($rule, $word)) 
-                return preg_replace($rule, $replacement, $word);
-        }
-
-        return false;
+        return self::add_cache('pluralize', $word, self::apply_rules($word, $plural));
+    
     }
 
     /**
@@ -77,109 +45,42 @@ class Phpr_Inflector
      * @param string $word English noun to singularize
      * @return string Singular noun.
      */
-    public static function singularize($word)
+    public static function singularize($word) 
     {
+        $result = self::get_cached_value('singularize', $word);
+        if ($result)
+            return $result;
+
         $singular = array (
-        '/(quiz)zes$/i' => '\\1',
-        '/(matr)ices$/i' => '\\1ix',
-        '/(vert|ind)ices$/i' => '\\1ex',
-        '/^(ox)en/i' => '\\1',
-        '/(alias|status)es$/i' => '\\1',
-        '/([octop|vir])i$/i' => '\\1us',
-        '/(cris|ax|test)es$/i' => '\\1is',
-        '/(shoe)s$/i' => '\\1',
-        '/(o)es$/i' => '\\1',
-        '/(bus)es$/i' => '\\1',
-        '/([m|l])ice$/i' => '\\1ouse',
-        '/(x|ch|ss|sh)es$/i' => '\\1',
-        '/(m)ovies$/i' => '\\1ovie',
-        '/(s)eries$/i' => '\\1eries',
-        '/([^aeiouy]|qu)ies$/i' => '\\1y',
-        '/([lr])ves$/i' => '\\1f',
-        '/(tive)s$/i' => '\\1',
-        '/(hive)s$/i' => '\\1',
-        '/([^f])ves$/i' => '\\1fe',
-        '/(^analy)ses$/i' => '\\1sis',
-        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\\1\\2sis',
-        '/([ti])a$/i' => '\\1um',
-        '/(n)ews$/i' => '\\1ews',
-        '/s$/i' => '',
-        );
-       
+            '/(quiz)zes$/i' => '\\1',
+            '/(matr)ices$/i' => '\\1ix',
+            '/(vert|ind)ices$/i' => '\\1ex',
+            '/^(ox)en/i' => '\\1',
+            '/(alias|status)es$/i' => '\\1',
+            '/([octop|vir])i$/i' => '\\1us',
+            '/(cris|ax|test)es$/i' => '\\1is',
+            '/(shoe)s$/i' => '\\1',
+            '/(status)$/i' => '\\1',
+            '/(o)es$/i' => '\\1',
+            '/(bus)es$/i' => '\\1',
+            '/([m|l])ice$/i' => '\\1ouse',
+            '/(x|ch|ss|sh)es$/i' => '\\1',
+            '/(m)ovies$/i' => '\\1ovie',
+            '/(s)eries$/i' => '\\1eries',
+            '/([^aeiouy]|qu)ies$/i' => '\\1y',
+            '/([lr])ves$/i' => '\\1f',
+            '/(tive)s$/i' => '\\1',
+            '/(hive)s$/i' => '\\1',
+            '/([^f])ves$/i' => '\\1fe',
+            '/(^analy)ses$/i' => '\\1sis',
+            '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\\1\\2sis',
+            '/([ti])a$/i' => '\\1um',
+            '/(n)ews$/i' => '\\1ews',
+            '/s$/i' => ''
+            );
 
-        $uncountable = array(
-            'equipment', 
-            'information', 
-            'rice', 
-            'money', 
-            'species', 
-            'series', 
-            'fish', 
-            'sheep',
-            'sms'
-        );
-
-        $irregular = array(
-            'person' => 'people',
-            'man' => 'men',
-            'child' => 'children',
-            'sex' => 'sexes',
-            'move' => 'moves'
-        );
-
-        $lowercased_word = strtolower($word);
-        foreach ($uncountable as $_uncountable)
-        {
-            if (substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable)
-                return $word;
-        }
-
-        foreach ($irregular as $_singular => $_plural)
-        {
-            if (preg_match('/('.$_plural.')$/i', $word, $arr)) 
-                return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word);
-        }
-
-        foreach ($singular as $rule => $replacement) 
-        {
-            if (preg_match($rule, $word)) 
-                return preg_replace($rule, $replacement, $word);
-        }
-
-        return $word;
-    }
-   
-    /**
-     * Get the plural form of a word if first parameter is greater than 1
-     * @param integer $numer_of_records
-     * @param string $word
-     * @return string Pluralized string when number of items is greater than 1
-     */
-    public static function conditional_plural($numer_of_records, $word)
-    {
-        return $numer_of_records > 1 ? Phpr_Inflector::pluralize($word) : $word;
-    }
-    
-    /**
-     * Converts an underscored or CamelCase word into a English
-     * sentence.
-     * 
-     * The titleize function converts text like "WelcomePage",
-     * "welcome_page" or  "welcome page" to this "Welcome
-     * Page".
-     * If second parameter is set to 'first' it will only
-     * capitalize the first character of the title.
-     * 
-     * @param string $word Word to format as tile
-     * @param string $uppercase If set to 'first' it will only uppercase the
-     * first character. Otherwise it will uppercase all
-     * the words in the title.
-     * @return string Text formatted as title
-     */
-    public static function titleize($word, $uppercase = '')
-    {
-        $uppercase = $uppercase == 'first' ? 'ucfirst' : 'ucwords';
-        return $uppercase(Phpr_Inflector::humanize(Phpr_Inflector::underscore($word)));
+        $result = self::apply_rules($word, $singular);
+        return self::add_cache('singularize', $word, $result);
     }
 
     /**
@@ -192,36 +93,122 @@ class Phpr_Inflector
      * @param string $word Word to convert to camel case
      * @return string UpperCamelCasedWord
      */
-    public static function camelize($word)
+    public static function camelize($word) 
     {
-        if (preg_match_all('/\/(.?)/',$word,$got))
+        $result = self::get_cached_value('camelize', $word);
+        if ($result)
+            return $result;
+        
+        if (preg_match_all('/\/(.?)/', $word, $got)) 
         {
-            foreach ($got[1] as $k=>$v)
-            {
-                $got[1][$k] = '::'.strtoupper($v);
-            }
-            $word = str_replace($got[0],$got[1],$word);
+            foreach ($got[1] as $k => $v)
+                $got[1][$k] = '::' . strtoupper($v);
+
+            $word = str_replace($got[0], $got[1], $word);
         }
-        return str_replace(' ','',ucwords(preg_replace('/[^A-Z^a-z^0-9^:]+/',' ',$word)));
+
+        $result = str_replace(' ', '', ucwords(preg_replace('/[^A-Z^a-z^0-9^:]+/', ' ', $word)));
+        $result{0} = strtolower($result{0});
+        
+        return self::add_cache('camelize', $word, $result);
     }
 
     /**
-     * Converts a word "into_it_s_underscored_version"
+     * Returns given word as PascalCased
      * 
-     * Convert any "CamelCased" or "ordinary Word" into an
-     * "underscored_word".
-     * 
-     * This can be really useful for creating friendly URLs.
-     * 
-     * @param string $word Word to underscore
-     * @return string Underscored word
+     * Converts a word like "send_email" to "SendEmail". It
+     * will remove non alphanumeric character from the word, so
+     * "who's online" will be converted to "WhoSOnline"
+     *
+     * @param string $word
+     * @return string
      */
-    public static function underscore($word)
+    public static function pascalize($word) 
     {
-        return  strtolower(preg_replace('/[^A-Z^a-z^0-9^\/]+/','_',
-        preg_replace('/([a-z\d])([A-Z])/','\1_\2',
-        preg_replace('/([A-Z]+)([A-Z][a-z])/','\1_\2',
-        preg_replace('/::/', '/',$word)))));
+        $result = self::get_cached_value('pascalize', $word);
+        if ($result)
+            return $result;
+        
+        if (preg_match_all('/\/(.?)/', $word, $got)) 
+        {
+            foreach ($got[1] as $k => $v)
+                $got[1][$k] = '::' . strtoupper($v);
+
+            $word = str_replace($got[0], $got[1], $word);
+        }
+
+        $result = str_replace(' ', '', ucwords(preg_replace('/[^A-Z^a-z^0-9^:]+/', ' ', $word)));
+        return self::add_cache('pascalize', $word, $result);
+    }
+
+    /**
+     * Same as camelize but first char is lowercased
+     * 
+     * Converts a word like "send_email" to "sendEmail". It
+     * will remove non alphanumeric character from the word, so
+     * "who's online" will be converted to "whoSOnline"
+     * 
+     * @param string $word Word to lowerCamelCase
+     * @return string Returns a lowerCamelCasedWord
+     */
+    public static function variablize($word)
+    {
+        $word = self::camelize($word);
+        return strtolower($word[0]).substr($word,1);
+    }
+
+    /**
+     * Converts a class name to its table name according to rails
+     * naming conventions.
+     * 
+     * Converts "Person" to "people"
+     * 
+     * @param string $class_name Class name for getting related table_name.
+     * @return string plural_table_name
+     */
+    public static function tableize($class_name) 
+    {
+        return self::pluralize(self::underscore($class_name));
+    }
+
+    /**
+     * Converts a table name to its class name according to rails
+     * naming conventions.
+     * 
+     * Converts "people" to "Person"
+     * 
+     * @param string $table_name Table name for getting related ClassName.
+     * @return string SingularClassName
+     */
+    public static function classify($table_name) 
+    {
+        return self::pascalize(self::singularize($table_name));
+    }
+    
+    /**
+     * Creates a URI slug from a given string.
+     * 
+     * Converts "Home Page" to "home-page"
+     *
+     * @param string $text
+     * @param string $separator
+     * @return string
+     */     
+    public static function slugify($string, $separator = '-') 
+    {
+        return strtolower(preg_replace(array('/[^-a-zA-Z0-9\s]/', '/[\s]/'), array('', $separator), $string));
+    }
+
+    /**
+     * Returns $class_name in underscored form, with "_id" tacked on at the end. 
+     * This is for use in dealing with the database.
+     * 
+     * @param string $class_name
+     * @return string
+     */
+    public static function foreign_key($class_name, $primary_key = 'id', $separate_class_name_and_id_with_underscore = true) 
+    {
+        return self::underscore(self::singularize($class_name)) . (($separate_class_name_and_id_with_underscore ? '_' : '') . $primary_key);
     }
 
     /**
@@ -239,56 +226,34 @@ class Phpr_Inflector
      * instead of just the first one.
      * @return string Human-readable word
      */
-    public static function humanize($word, $uppercase = '')
+    public static function humanize($word, $uppercase = '') 
     {
-        $uppercase = $uppercase == 'all' ? 'ucwords' : 'ucfirst';
-        return $uppercase(str_replace('_',' ',preg_replace('/_id$/', '',$word)));
+        $uppercase = ($uppercase == 'all') ? 'ucwords' : 'ucfirst';
+        $result = self::get_cached_value('humanize'.$uppercase, $word);
+        if ($result)
+            return $result;
+            
+        $result = $uppercase(str_replace('_', ' ', preg_replace('/_id$/', '', $word)));
+
+        return self::add_cache('humanize'.$uppercase, $word, $result);
     }
 
-    /**
-     * Same as camelize but first char is lowercased
-     * 
-     * Converts a word like "send_email" to "sendEmail". It
-     * will remove non alphanumeric character from the word, so
-     * "who's online" will be converted to "whoSOnline"
-     * 
-     * @param string $word Word to lowerCamelCase
-     * @return string Returns a lowerCamelCasedWord
-     */
-    public static function variablize($word)
+    public static function demodulize($classname) 
     {
-        $word = Phpr_Inflector::camelize($word);
-        return strtolower($word[0]).substr($word,1);
-    }
+        $result = self::get_cached_value('demodulize', $classname);
+        if ($result)
+            return $result;
+        
+        if (($classname != 'Controller') && Util::ends_with($classname, 'Controller'))
+            $classname = substr($classname, 0, -10);
 
-    /**
-     * Converts a class name to its table name according to rails
-     * naming conventions.
-     * 
-     * Converts "Person" to "people"
-     * 
-     * @param string $class_name Class name for getting related table_name.
-     * @return string plural_table_name
-     */
-    public static function tableize($class_name)
-    {
-        return Phpr_Inflector::pluralize(Phpr_Inflector::underscore($class_name));
-    }
+        if (($classname != 'Behavior') && Util::ends_with($classname, 'Behavior'))
+            $classname = substr($classname, 0, -8);
 
-    /**
-     * Converts a table name to its class name according to rails
-     * naming conventions.
-     * 
-     * Converts "people" to "Person"
-     * 
-     * @param string $table_name Table name for getting related ClassName.
-     * @return string SingularClassName
-     */
-    public static function classify($table_name)
-    {
-        return Phpr_Inflector::camelize(Phpr_Inflector::singularize($table_name));
+        $result = strtolower($classname);
+        return self::add_cache('demodulize', $word, $result);
     }
-
+    
     /**
      * Converts number to its ordinal English form.
      * 
@@ -321,17 +286,6 @@ class Phpr_Inflector
         }
     }
 
-    public static function demodulize($module_name)
-    {
-        $module_name = preg_replace('/^.*::/','',$module_name);
-        return Phpr_Inflector::humanize(Phpr_Inflector::underscore($module_name));
-    }
-
-    public static function modulize($module_description)
-    {
-        return Phpr_Inflector::camelize(Phpr_Inflector::singularize($module_description));
-    }
-
     /**
      * Transforms a string to its unaccented version. 
      * This might be useful for generating "friendly" URLs
@@ -349,30 +303,88 @@ class Phpr_Inflector
             'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'þ'=>'t', 'ÿ'=>'y');
         return str_replace(array_keys($map), array_values($map), $text);
     }
-    
+
     /**
-     * Creates a URI slug from a given string.
+     * Converts a word "into_it_s_underscored_version"
      * 
-     * Converts "Home Page" to "home-page"
+     * Convert any "CamelCased" or "ordinary Word", or "ordinary-word" into an "underscored_word".
+     * This can be really useful for creating friendly URLs.
      *
-     * @param string $text
-     * @param string $separator
-     * @return string
-     */       
-    public static function slugify($text, $separator = '_')
-    {
-        return trim(Phpr_Inflector::underscore(Phpr_Inflector::unaccent($text)), $separator);
-    }
-    
-    /**
-     * Returns $class_name in underscored form, with "_id" tacked on at the end. 
-     * This is for use in dealing with the database.
-     * 
-     * @param string $class_name
+     * @param string $word
      * @return string
      */
-    public static function foreign_key($class_name, $primary_key = 'id', $separate_class_name_and_id_with_underscore = true)
+    public static function underscore($word) 
     {
-        return Phpr_Inflector::underscore(Phpr_Inflector::demodulize($class_name)).($separate_class_name_and_id_with_underscore ? "_" . $primary_key : $primary_key);
+        $result = self::get_cached_value('underscore', $word);
+        if($result)
+            return $result;
+        
+        $result = strtolower(preg_replace('/[^A-Z^a-z^0-9^\/]+/', '_',
+        preg_replace('/([a-z\d])([A-Z])/','\1_\2',
+        preg_replace('/([A-Z]+)([A-Z][a-z])/','\1_\2',
+        preg_replace('/::/', '/',$word)))));
+
+        return self::add_cache('underscore', $word, $result);
+    }
+
+    /**
+     * Apply language rules specified in $rules
+     *
+     * @param string $word
+     * @param mixed[] $rules
+     * @return string
+     */
+    protected static function apply_rules($word, $rules) 
+    {
+        $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep', 'sms');
+
+        $irregular = array(
+            'person' => 'people',
+            'man' => 'men',
+            'child' => 'children',
+            'sex' => 'sexes',
+            'move' => 'moves'
+        );
+
+        $lowercased_word = strtolower($word);
+
+        foreach ($uncountable as $_uncountable)
+        {
+            if (substr($lowercased_word, (-1 * strlen($_uncountable))) == $_uncountable)
+                return $word;
+        }
+
+        foreach ($irregular as $_plural => $_singular)
+        {
+            if (preg_match('/(' . $_plural . ')$/i', $word, $arr))
+                return preg_replace('/(' . $_plural . ')$/i', substr($arr[0], 0, 1) . substr($_singular, 1), $word);
+        }
+
+        foreach ($rules as $rule => $replacement) 
+        {
+            if (preg_match($rule, $word))
+                return preg_replace($rule, $replacement, $word);
+        }
+
+        return $word;
+    }
+
+    protected static function get_cached_value($function, $argument)
+    {
+        if (!array_key_exists($function, self::$cache))
+            return null;
+            
+        if (!array_key_exists($argument, self::$cache[$function]))
+            return null;
+            
+        return self::$cache[$function][$argument];
+    }
+    
+    protected static function add_cache($function, $argument, $value)
+    {
+        if (!array_key_exists($function, self::$cache))
+            self::$cache[$function] = array();
+            
+        return self::$cache[$function][$argument] = $value;
     }
 }

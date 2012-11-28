0c0b69417d4cf70c72691b38f5fc2f650bc2914c
Sam Georges <sam@daftspunk.com>
2012-11-28 20:08:32 +1100

    Set up init further

--- system/core/init.php
+++ system/core/init.php
@@ -1,6 +1,6 @@
 <?php
 
-// Load initial configuration. This is included more than once.
+// Load initial configuration. This is included again below.
 if ($path = realpath(PATH_APP . '/config/config.php'))
     include($path); 
 
@@ -40,6 +40,26 @@ require_once('exceptions.php');
 // Process resource requests (may terminate thread)
 Phpr_Response::process_resource_request();
 
+// Event handling
+Phpr::$events = new Phpr_Events();
 
-// Init others
-Phpr::$config = new Phpr_Config();
+// Reponse object
+Phpr::$response = new Phpr_Response();
+
+// Session handling
+Phpr::$session = new Phpr_Session();
+
+// Security system
+Phpr::$security = new Phpr_Security();
+
+// Internal deprecation
+Phpr::$deprecate = new Phpr_Deprecate();
+
+// Configure the application and initialize the request object
+if (Phpr::$router === null)
+    Phpr::$router = new Phpr_Router();
+
+// Load config for usage
+if ($path = realpath(PATH_APP . '/config/config.php'))
+    include($path);
+

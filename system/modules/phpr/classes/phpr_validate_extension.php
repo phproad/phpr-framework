75d9485e68da2a02dfc68adfe7382649902dbe99
Sam Georges <sam@daftspunk.com>
2012-12-01 10:28:22 +1100

    Set up base class for extension validation

--- system/modules/phpr/classes/phpr_validate_extension.php
+++ system/modules/phpr/classes/phpr_validate_extension.php
@@ -1,11 +1,19 @@
 <?php
 
 /**
- * This empty class ensures a proper naming standard
+ * Base class for extension validation
  * @see Phpr_Extension
  */
 
-class Phpr_Extendable extends Phpr_Extension
+class Phpr_Validate_Extension extends Phpr_Extension
 {
+
+    public function _execute_validation($method, $name, $value)
+    {
+        if (method_exists($this, $method))
+            return $this->$method($name, $value);
+
+        throw new Phpr_SystemException('Validation method '.$method.' not found in '.get_class($this));
+    }
 
 }

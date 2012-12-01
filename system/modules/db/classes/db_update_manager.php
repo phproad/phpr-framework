e25268f080ce2e639dace4d8319094e61c9e9323
Sam Georges <sam@daftspunk.com>
2012-12-01 13:11:52 +1100

    Add update manager

---
+++ system/modules/db/classes/db_update_manager.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PHPR Update manager
+ * Manages module update chain
+ */
+class Db_Update_Manager 
+{
+
+    private static $updates = null;
+
+    // Updates all mods
+    public static function update()
+    {
+
+    }
+
+    // Updates a single module
+    public static function update_module($module_id)
+    {
+
+    }
+
+}

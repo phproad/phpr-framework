ebb5b1c7c78e5c6f5483bc853a45aa1591af1560
Sam Georges <sam@daftspunk.com>
2013-04-01 18:01:52 +1100

    Add front end framework form definition and validation libs

---
+++ assets/scripts/js/phpr.form.js
@@ -0,0 +1,228 @@
+// Leaving this here for now... should go in phpr.js
+
+
+window.PHPR = { };
+
+PHPR.doSomething = function(element) {
+ 
+};
+
+$.phpr = PHPR;
+
+
+
+$.fn.phpr = function() {
+
+	var self = this;
+
+	var o = {};
+
+	o.form = function() {
+		return $.phpr.form(self);
+	};
+
+	o.validate = function() {
+		return $.phpr.validate(self);
+	};
+
+	return o;
+};
+
+// End testing code
+
+/**
+ * PHPR Form
+ * 
+ * Usage: See PHPR Validate
+ */
+
+
+(function($) {
+
+	PHPR.form = function(element) {
+
+		var o = {},
+			_form = element,
+			_fields = {};
+
+		o.defineFields = function(object) {
+			object.prototype.defineField = function(field, name) {
+				return o.defineField(field, name);
+			}
+			new object();
+			return this;
+		}
+
+		o.defineField = function(field, name) {
+			var fieldObj = new PHPR.formFieldDefinition(_form, field, name);
+			_fields[field] = fieldObj;
+			return fieldObj;
+		};
+
+		o.validate = function(element, options) {
+			var form = element ? $(element) : _form;
+			
+			options = $.extend(true, {
+				rules: this.getFieldRules(),
+				messages: this.getFieldMessages()
+			}, options);
+
+			return new PHPR.validate(form, options);
+		}
+
+		// Shorthand helper 
+		o.valid = function(element) {
+			return o.validate(element).valid();
+		};
+
+		o.getFieldRules = function() {
+			var allRules = {};
+			$.each(_fields, function(field, fieldObj){
+				allRules[field] = fieldObj.getRules();
+			});
+			return allRules;
+		};
+
+		o.getFieldMessages = function() {
+			var allMessages = {};
+			$.each(_fields, function(field, fieldObj){
+				allMessages[field] = fieldObj.getMessages();
+			});
+			return allMessages;
+		};	
+
+		return o;
+	}
+
+
+	PHPR.formFieldDefinition = function(element, field, name) {
+
+		var _rules = {},
+			_messages = {},
+			_field = field,
+			_name = name,
+			_form = element;
+
+		// Validation Rules
+		// 
+		this.required = function(message) {
+			_rules.required = true;
+			_messages.required = message;
+			return this;
+		};
+
+		// Requires at least X (minFilled) inputs (element class) populated 
+		this.requiredMulti = function(minFilled, elementClass, message) {
+			_rules.required_multi = [minFilled, elementClass];
+			_messages.required_multi = message;
+			return this;
+		};
+
+		this.number = function(message) {
+			_rules.number = true;
+			_messages.number = message;
+			return this;
+		};
+
+		this.phone = function(message) {
+			_rules.phone = true;
+			_messages.phone = message;
+			return this;
+		};
+
+		this.email = function(message) {
+			_rules.email = true;
+			_messages.email = message;
+			return this;
+		};
+
+		this.date = function(message) {
+			_rules.ahoyDate = true;
+			_messages.ahoyDate = message;
+			return this;
+		};
+
+		this.url = function(message) {
+			_rules.fullUrl = true;
+			_messages.fullUrl = message;
+			return this;
+		};
+
+		this.range = function(range, message) {
+			_rules.range = range;
+			_messages.range = message;
+			return this;
+		};
+
+		this.min = function(min, message) {
+			_rules.min = min;
+			_messages.min = message;
+			return this;
+		};
+
+		this.max = function(max, message) {
+			_rules.max = max;
+			_messages.max = message;
+			return this;
+		};
+
+		this.minWords = function(words, message) {
+			_rules.minWord = words;
+			_messages.minWord = message;
+			return this;
+		};
+
+		this.matches = function(field, message) {
+			_rules.equalTo = field;
+			_messages.equalTo = message;
+			return this;
+		};
+
+		// Submits a remote action using phpr().post()
+		this.action = function(action, message) {
+			_rules.ahoyRemote = { action:action };
+			_messages.ahoyRemote = message;
+			return this;
+		};
+
+		// Setters
+		// 
+
+		// Manually sets a rule
+		this.setRule = function(rule, value) {
+			_rules[rule] = value;
+			return this;
+		};
+
+		// Manually sets a message
+		this.setMessage = function(rule, value) {
+			_messages[rule] = value;
+			return this;
+		};
+		
+		// Manually sets rules
+		this.setRules = function(rules) {
+			_rules = $.extend(true, _rules, rules);
+			return this;
+		};
+
+		// Manually sets messages
+		this.setMessages = function(messages) {
+			_messages = $.extend(true, _messages, messages);
+			return this;
+		};
+
+		// Getters
+		// 
+
+		this.getRules = function() {
+			return _rules;
+		}
+
+		this.getMessages = function() {
+			return _messages;
+		}
+
+	}
+
+})(jQuery);
---
+++ assets/scripts/js/phpr.validate.js
@@ -0,0 +1,102 @@
+/**
+ * PHPR Validate
+ * 
+ * Usage:
+ * 
+ *   Page.loginForm = $('#login-form').phpr().form();
+ *   Page.loginForm.defineFields(function(){
+ *   	this.defineField('login', 'Login Name').required('You must enter a login name');
+ *   	this.defineField('password', 'Password').required();
+ *   });
+ *   
+ *   if (Page.loginForm.valid())
+ *   	doSomething();
+ * 
+ *   Page.loginForm.validate().action('on_action')
+ *   	.success(function() { alert('Done!'); });
+ *   	
+ *   	
+ * Deferring:
+ *   
+ *   Page.loginForm = $.phpr.form();
+ *   Page.loginForm.defineField('name', 'Name').required('You must enter a name');
+ *   
+ *   (later)
+ *   
+ *   Page.loginForm.validate('#login-form').action('on_action')
+ *   	.success(function() { alert('Done!'); });
+ * 
+ */
+
+(function($) {
+
+	PHPR.validateDefaults = {
+		ignore:":not(:visible):not(:disabled)",
+		onkeyup: false,
+		submitHandler: function(form) {
+			form.submit();
+		}
+	};
+
+	PHPR.validate = function(element, options) {
+		var o = {},
+			_options = {},
+			_form = element,
+			_on_success = null,
+			_jv_object,
+			_jv_defaults = PHPR.validateDefaults;
+
+		if (element.length == 0)
+			throw 'PHPR Validate: Unable to find form element';
+
+		_options = $.extend(true, _jv_defaults, options);
+		_jv_object = _form.validate(_options);
+
+		o.valid = function() {
+			return _jv_object.valid();
+		}
+
+		o.success = function(func) {
+			_jv_object.settings.submitHandler = _on_success = func;
+			return this;
+		}
+
+		o.action = function(ajaxHandler) {
+			var postObj = Ahoy.post(_form).action(ajaxHandler);
+
+			this.success(function() {
+				postObj.send();
+			});
+
+			return postObj;
+		}
+
+		o.resetFormFields = function() {
+			_jv_object.settings.rules = null;
+			_jv_object.settings.messages = null;
+			return this;
+		}
+
+		o.addFormFields = function(formObj) {
+			_jv_object.settings.rules = $.extend(true, _jv_object.settings.rules, formObj.getFieldRules());
+			_jv_object.settings.messages = $.extend(true, _jv_object.settings.messages, formObj.getFieldMessages());
+			return this;
+		}
+
+		o.removeFormFields = function(formObj) {
+			$.each(formObj.getFieldRules(), function(field, fieldObj){
+				if (_jv_object.settings.rules[field])
+					_jv_object.settings.rules[field] = null;
+			});
+
+			$.each(formObj.getFieldMessages(), function(field, fieldObj){
+				if (_jv_object.settings.messages[field])
+					_jv_object.settings.messages[field] = null;
+			});
+			return this;
+		}
+
+		return o;
+	}
+
+})(jQuery);

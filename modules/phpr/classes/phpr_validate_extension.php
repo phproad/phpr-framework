0641e5a6ce657df57d42ae994091da13257cd07d
Sam Georges <sam@daftspunk.com>
2013-06-11 19:36:28 +1000

    Spaces -> tabs

--- modules/phpr/classes/phpr_validate_extension.php
+++ modules/phpr/classes/phpr_validate_extension.php
@@ -8,12 +8,12 @@
 class Phpr_Validate_Extension extends Phpr_Extension
 {
 
-    public function _execute_validation($method, $name, $value)
-    {
-        if (method_exists($this, $method))
-            return $this->$method($name, $value);
+	public function _execute_validation($method, $name, $value)
+	{
+		if (method_exists($this, $method))
+			return $this->$method($name, $value);
 
-        throw new Phpr_SystemException('Validation method '.$method.' not found in '.get_class($this));
-    }
+		throw new Phpr_SystemException('Validation method '.$method.' not found in '.get_class($this));
+	}
 
 }

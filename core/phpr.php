1a928ad36a053da7a0702dbe2e68d707c6b2be43
Sam Georges <sam@daftspunk.com>
2013-06-11 19:47:14 +1000

    Spaces -> tabs

--- core/phpr.php
+++ core/phpr.php
@@ -8,43 +8,43 @@
 
 class Phpr 
 {
-    // Phpr_Config
-    public static $config;
+	// Phpr_Config
+	public static $config;
 
-    // Phpr_ClassLoader
-    public static $class_loader;
+	// Phpr_ClassLoader
+	public static $class_loader;
 
-    // Phpr_Error_Log
-    public static $error_log;
+	// Phpr_Error_Log
+	public static $error_log;
 
-    // Phpr_TraceLog
-    public static $trace_log;
+	// Phpr_Trace_Log
+	public static $trace_log;
 
-    // Phpr_Router
-    public static $router;
+	// Phpr_Router
+	public static $router;
 
-    // Phpr_Response
-    public static $response;
+	// Phpr_Response
+	public static $response;
 
-    // Phpr_Request
-    public static $request;
+	// Phpr_Request
+	public static $request;
 
-    // Phpr_Localization
-    public static $locale;
+	// Phpr_Localization
+	public static $locale;
 
-    // Phpr_Security
-    public static $security;
+	// Phpr_Security
+	public static $security;
 
-    // Phpr_Security
-    public static $frontend_security;
+	// Phpr_Security
+	public static $frontend_security;
 
-    // Phpr_Session
+	// Phpr_Session
 
-    public static $session;
+	public static $session;
 
-    // Phpr_Events
-    public static $events;
-    
-    // Phpr_Deprecate
-    public static $deprecate;
+	// Phpr_Events
+	public static $events;
+	
+	// Phpr_Deprecate
+	public static $deprecate;
 }

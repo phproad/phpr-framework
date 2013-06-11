5e625f3e090c41de98e1b2d179ac28d674f274f6
Sam Georges <sam@daftspunk.com>
2013-06-11 19:46:52 +1000

    Complete initialize workflow

--- core/init.php
+++ core/init.php
@@ -1,11 +1,29 @@
 <?php
 
+// Fix for XDebug aborting threads > 100 nested
+ini_set('xdebug.max_nesting_level', 300);
+
 // Load initial configuration. This is included again below.
 if ($path = realpath(PATH_APP . '/config/config.php'))
-    include($path); 
+	include($path); 
 
 // Core PHPR class
 require_once('phpr.php');
+
+// Add support for get_called_class() in 5.2
+// 
+
+if (!function_exists('get_called_class')) {
+	function get_called_class() 
+	{
+		$t = debug_backtrace(); 
+		$t = $t[0];
+		if (isset($t['object']) && $t['object'] instanceof $t['class']) {
+			return get_class( $t['object'] );
+		}
+		return false;
+	}
+}
 
 // Initialize auto class loading
 // 
@@ -16,22 +34,22 @@ Phpr::$class_loader = new Phpr_ClassLoader();
 
 function phpr_autoload_internal($name) 
 {
-    if (!Phpr::$class_loader->load($name)) 
-    {
-        // Load failed
-    }
+	if (!Phpr::$class_loader->load($name)) 
+	{
+		// Load failed
+	}
 }
 
 if (function_exists('spl_autoload_register')) 
 {
-    spl_autoload_register('phpr_autoload_internal');
+	spl_autoload_register('phpr_autoload_internal');
 } 
 else 
 {
-    function __autoload($name) 
-    {
-        phpr_autoload_internal($name);
-    }
+	function __autoload($name) 
+	{
+		phpr_autoload_internal($name);
+	}
 }
 
 // Exception handling
@@ -56,10 +74,77 @@ Phpr::$security = new Phpr_Security();
 Phpr::$deprecate = new Phpr_Deprecate();
 
 // Configure the application and initialize the request object
-if (Phpr::$router === null)
-    Phpr::$router = new Phpr_Router();
+Phpr::$router = new Phpr_Router();
 
 // Load config for usage
 if ($path = realpath(PATH_APP . '/config/config.php'))
-    include($path);
+	include($path);
+
+// Application controller
+if ($path = Phpr::$class_loader->find_path('controllers/application.php'))
+	include_once($path);
+
+// Initialize script
+if ($path = Phpr::$class_loader->find_path('init/init.php'))
+	include_once($path);
+
+// Config object
+Phpr::$config = new Phpr_Config();
+
+// Request object
+Phpr::$request = new Phpr_Request();
+
+// Error log
+Phpr::$error_log = new Phpr_Error_Log();
+
+// Trace log	
+Phpr::$trace_log = new Phpr_Trace_Log();
+
+// Localization
+Phpr::$locale = new Phpr_Localization();
+
+// Run modules initialization scripts
+//
+
+function init_phpr_modules() 
+{
+	$paths = Phpr::$class_loader->find_paths('modules');
+
+	foreach ($paths as $path) 
+	{
+		$iterator = new DirectoryIterator($path);
+		
+		foreach ($iterator as $directory) 
+		{
+			if (!$directory->isDir() || $directory->isDot())
+				continue;
+			
+			if (!file_exists($init_dir = $directory->getPathname() . '/init'))
+				continue;
+			
+			$file_iterator = new DirectoryIterator($init_dir);
+			
+			foreach ($file_iterator as $file) 
+			{
+				if (!$file->isFile())
+					continue;
+					
+				$info = pathinfo($file->getPathname());
+				
+				if (isset($info['extension']) && $info['extension'] == PHPR_EXT)
+					include($file->getPathname());
+			}
+		}
+	}
+}
+
+init_phpr_modules();
+
+Phpr::$session->restore_db_data();
+
+/**
+ * Execute requested action
+ */ 
+if (empty($PHPR_INIT_ONLY))
+	Phpr::$response->open(Phpr::$request->get_current_uri(true));
 

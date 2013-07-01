80a5bd9766a9a03596faac45aaef3915bfa71ac3
Sam Georges <sam@daftspunk.com>
2013-07-01 17:35:31 +1000

    Add namespacing to core + exceptions

--- core/init.php
+++ core/init.php
@@ -3,6 +3,47 @@
 // Fix for XDebug aborting threads > 100 nested
 ini_set('xdebug.max_nesting_level', 300);
 
+//
+// PHP functions that should really exist
+//
+
+// Creates a unified class id
+if ( ! function_exists('get_class_id')) 
+{
+	function get_class_id($obj)
+	{
+		if (!is_string($obj))
+			$class_name = get_class($obj);
+		else
+			$class_name = $obj;
+					
+		$class_name = str_replace('\\', '_', get_class($obj));
+		return $class_name;
+	}
+}
+
+// Obtains an object class name without namespaces
+if ( ! function_exists('get_real_class')) 
+{
+	function get_real_class($obj) 
+	{
+		if (!is_string($obj))
+			$class_name = get_class($obj);
+		else
+			$class_name = $obj;
+
+		if (preg_match('@\\\\([\w]+)$@', $class_name, $matches)) {
+			$class_name = $matches[1];
+		}
+
+		return $class_name;
+	}
+}
+
+//
+// Configuration
+//
+
 // Load initial configuration. This is included again below.
 if ($path = realpath(PATH_APP . '/config/config.php'))
 	include($path); 
@@ -16,7 +57,7 @@ require_once('phpr.php');
 
 require_once('classloader.php');
 
-Phpr::$class_loader = new Phpr_ClassLoader();
+Phpr::$class_loader = new \Phpr\ClassLoader();
 
 function phpr_autoload_internal($name) 
 {
@@ -60,7 +101,7 @@ Phpr::$security = new Phpr_Security();
 Phpr::$deprecate = new Phpr_Deprecate();
 
 // Configure the application and initialize the request object
-Phpr::$router = new Phpr_Router();
+Phpr::$router = new Phpr\Router();
 
 // Load config for usage
 if ($path = realpath(PATH_APP . '/config/config.php'))
@@ -105,7 +146,8 @@ function init_phpr_modules()
 			if (!$directory->isDir() || $directory->isDot())
 				continue;
 
-			if (!file_exists($module_file = $directory->getPathname() . '/classes/' . basename($directory->getPathname()) . '_module.php'))
+			if ( ! file_exists($module_file = $directory->getPathname() . '/classes/module.php') 
+				&& ! file_exists($module_file = $directory->getPathname() . '/classes/' . basename($directory->getPathname()) . '_module.php'))				
 				continue;
 			
 			if (!file_exists($init_dir = $directory->getPathname() . '/init'))
@@ -119,7 +161,7 @@ function init_phpr_modules()
 					continue;
 					
 				$info = pathinfo($file->getPathname());
-				
+
 				if (isset($info['extension']) && $info['extension'] == PHPR_EXT)
 					include($file->getPathname());
 			}
